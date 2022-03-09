eef36beeb4e50d84984e3e6bec8e423bd9a68a3a
kelvi <ckmartin1995@gmail.com>
2022-03-09 11:57:05 -0500

    added weather api key
    i dont care that it's public

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: '', // Write here your API Key
+  weatherKey: '9af221c258ebf722f294a4f2d522563f', // Write here your API Key
   weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: 'F', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
